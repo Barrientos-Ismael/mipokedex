bd344241c63582fb3ed47cf6f5bc1c6666513ba9
Barrientos Ismael <ismaeljb92@gmail.com>
2021-05-18 16:47:45 -0300

    mejorando CSS, corecciones en JS, agregando eventos, mejora del HTML

--- ConAjax/pokescript.js
+++ ConAjax/pokescript.js
@@ -1,5 +1,6 @@
 const idPokeLista = document.getElementById("pokelista"); // id => pokelista
 const idShowPokemon = document.getElementById("showPokemon");
+const guessSubmit = document.getElementById("guessSubmit");
 const pokeimg = document.getElementById("pokemon-img");
 const pokename = document.getElementById("pokemon-name");
 const poketype = document.getElementById("pokemon-type");
@@ -8,6 +9,7 @@ var prevPage = null;
 var nextPage = null; 
 const apiRoot = "https://pokeapi.co/api/v2/pokemon/";
 var actual = 1;
+
 
 /**me guardo las variables de forma global del paginado */
 
@@ -37,8 +39,11 @@ function stateChangeHandlerSingle(){
             id: pokeObj.id,
             image: pokeObj.sprites['front_default'],
             type: pokeObj.types.map((type) => type.type.name),
-            stats: pokeObj.stats.map((stats) => stats.stat.name),
-            statsValue: pokeObj.stats.map((stats) => stats.base_stat)
+            stats: pokeObj.stats.map((stats)=>{
+                var array = [];
+                array.push(stats.stat.name+': '+stats.base_stat);
+                return array;
+            })
         }
         console.log(pokemon);
         actual = pokemon.id;
@@ -86,7 +91,7 @@ function agregarPokemon(pokemon){
     poketype.textContent = "type: "+pokemon.type;
     for (var i=0; i<pokemon.stats.length;i++){
         var node = document.createElement("li");
-        node.textContent=(pokemon.stats[i]+": "+pokemon.statsValue[i]);
+        node.textContent=(pokemon.stats[i]);
         pokestats.appendChild(node);
     };
 }
@@ -126,8 +131,158 @@ function siguientePoke(){
 function elegirPoke(dato){ //Llamado para seleccionar un pokemon por el nombre
     singleRequest (apiRoot+dato);
 }
+
+function buscarPoke(){
+    let dato = String(guessField.value);
+    elegirPoke(dato);
+}
  
 //Inicio 
-
+addEventListener('click', buscarPoke);
 listRequest ("https://pokeapi.co/api/v2/pokemon?limit=12&offset=0");
 singleRequest (apiRoot+actual);
+
+const apiRoot = "https://pokeapi.co/api/v2/pokemon/";
+const imageRoot = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/';
+const imageRoot2 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+const idPokeLista = document.getElementById("pokelista"); // id => pokelista
+const idShowPokemon = document.getElementById("showPokemon"); // id => showPokemon
+const pokeimg = document.querySelector(".card-image");
+const pokeName = document.querySelector(".card-title");
+const pokeType = document.querySelector(".card-subtitle");
+
+const guessField = document.getElementById('guessField');
+const guessSubmit = document.getElementById('guessSubmit');
+
+var prevPage = null;
+var nextPage = null;
+var actual = 1;
+
+guessField.addEventListener("keyup", function (event) {
+
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById("guessSubmit").click();
+    }
+})
+guessField.addEventListener("click", function (event) {
+    guessField.placeholder = 'Ingrese Id o Nombre';
+})
+guessSubmit.addEventListener("click", function (event) {
+    guessField.value = '';
+    guessField.placeholder = 'Busqueda';
+    guessField.blur();
+})
+
+/**me guardo las variables de forma global del paginado */
+
+function listRequest(value) { //Hace el pedido a la Api - value es la URL - para la lista de pokemones
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", value, true);
+    xhttp.send();
+    xhttp.onreadystatechange = stateChangeHandlerLista;
+}
+
+function singleRequest(value) { //Hace el pedido a la Api - value es la URL - para un pokemon
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", value, true);
+    xhttp.send();
+    xhttp.onreadystatechange = stateChangeHandlerSingle;
+}
+
+function stateChangeHandlerSingle() {
+    if (this.readyState == 4) {
+        if (this.status == 200) {
+            let pokeObj = JSON.parse(this.responseText);
+            console.log(pokeObj);
+            let img= "";
+            if (pokeObj.id >= 650){
+                img=imageRoot2+pokeObj.id+".png";
+            } else img=imageRoot+pokeObj.id+".svg";
+            const pokemon = {
+                name: pokeObj.name,
+                id: pokeObj.id,
+                image: img,
+                type: pokeObj.types.map((type) => type.type.name)
+            }
+            actual = pokemon.id;  // asi podemos conjugar la elección del pokemon entre el "atras-siguiente", y elegirlo directamente
+            pokeimg.src = `${pokemon.image}`;
+            pokeName.textContent = `${pokemon.id}. ${pokemon.name}`;
+            pokeType.textContent = `Type: ${pokemon.type}`;
+        } else if (this.status == 404) {
+            alert('No existe ese pokemon');
+        }
+    }
+}
+
+
+function stateChangeHandlerLista() {
+    if (this.readyState == 4 && this.status == 200) {
+        let pokeObj = JSON.parse(this.responseText);
+        limpiarLista();
+        console.log(pokeObj); //Verificacion
+        nextPage = pokeObj.next;
+        prevPage = pokeObj.previous; //Seteo las paginas
+        let arrayPokes = pokeObj.results; //Se guarda en un arraglo los pokemones
+        console.log(arrayPokes);
+        for (let i = 0; i < arrayPokes.length; i++) {
+            let miPokemon = arrayPokes[i].name; //guardo el nombre del pokemon
+            var node = document.createElement("li");
+            node.classList.add("nodoPokeLista");
+            node.textContent=(miPokemon);
+            node.addEventListener("click", function () {
+                elegirPoke(miPokemon);
+            }); // Agregando evento al clickear un pokemon
+            idPokeLista.appendChild(node); //agrego el elemento creado a la lista <ul>
+        }
+    }
+}
+
+function limpiarLista(){
+    while (idPokeLista.firstChild){
+        idPokeLista.removeChild(idPokeLista.firstChild);
+    }
+}
+
+function siguientePag() {
+    if (nextPage != null) {
+        listRequest(nextPage);
+    }
+}
+
+function anteriorPag() {
+    if (prevPage != null) {
+        listRequest(prevPage);
+    }
+}
+
+function anteriorPoke() {
+    if (actual > 1) {
+        actual--;
+        singleRequest(apiRoot + actual);
+    }
+}
+
+function siguientePoke() {
+    if (actual < 1118) {
+        actual++;
+        singleRequest(apiRoot + actual);
+    }
+}
+
+function elegirPoke(dato) { //Llamado para seleccionar un pokemon por el nombre
+    console.log(dato);
+    singleRequest(apiRoot + dato);
+}
+
+function buscarPoke() {
+    let dato = String(guessField.value).toLowerCase();
+    if(dato != ''){
+        elegirPoke(dato);
+    }else{
+        alert('Ingresá un valor antes de buscar.')
+    }
+};
+
+listRequest("https://pokeapi.co/api/v2/pokemon?limit=18&offset=0");
+singleRequest(apiRoot + actual);
--- ConFetch/pokescript.js
+++ ConFetch/pokescript.js
@@ -1,38 +1,144 @@
-const pokedex = document.getElementById("pokedex");
+const apiRoot = 'https://pokeapi.co/api/v2/pokemon/';
+const idpokedex = document.querySelector('.pokedex');
+const idPokeLista = document.querySelector('.pokelista');
+const pokeimg = document.querySelector(".pokemon-img");
+const pokename = document.querySelector(".pokemon-name");
+const poketype = document.querySelector(".pokemon-type");
+const pokestats = document.querySelector(".pokemon-stats");
+const nodos = document.getElementsByClassName('nodoPokeLista');
+const nodosStats = document.getElementsByClassName('stats');
 
-console.log(pokedex);
+var prevPage = null;
+var nextPage = null; 
+var actual = 1;
+const limit = 18;
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 6; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
+guessField.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById("guessSubmit").click();
     }
-    Promise.all(promises).then((results) =>{
-        const pokemon = results.map((data) => ({
+})
+
+guessField.addEventListener("click", function (event) {
+    guessField.placeholder = 'Ingrese Id o Nombre';
+})
+
+guessSubmit.addEventListener("click", function (event) {
+    guessField.value = '';
+    guessField.placeholder = 'Busqueda';
+    guessField.blur();
+})
+
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+function agregarPokemon(pokemon){
+    pokeimg.src = pokemon.image;
+    let pokeid = pokemon.id;
+    pokename.textContent = pokeid.toString().padStart(3, '0')+". "+pokemon.name;
+    poketype.textContent = "type: "+pokemon.type;
+    if (pokestats.hasChildNodes()) {
+        for (var i=0; i<pokemon.stats.length;i++){
+            nodosStats[i].textContent=(pokemon.stats[i]+": "+pokemon.statsValue[i]);
+        };
+    } else {
+        for (var i=0; i<pokemon.stats.length;i++){
+            var node = document.createElement("li");
+            node.classList.add("stats");
+            node.textContent=(pokemon.stats[i]+": "+pokemon.statsValue[i]);
+            pokestats.appendChild(node);
+        };
+    }      
+}
+
+const fetchPokemonList = url => {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const { results, previous, next } = data;
+            prevPage = previous,
+            nextPage = next;
+            for (let i=0; i < data.results.length; i++){
+                let miPokemon = data.results[i];
+                if (miPokemon) {
+                    const {name, url} = miPokemon;
+                    const urlArray = url.split('/');
+                    const id = urlArray[urlArray.length -2];
+                    nodos[i].textContent= id + '. ' + capitalize(name);
+                    nodos[i].addEventListener("click", function () {
+                         elegirPoke(this.textContent);
+                    })
+                } else {
+                    node.textContent='';
+                }
+            }
+        })
+}
+
+const fetchPokeData = url => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = {
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
-            type: data.types.map((type) => type.type.name).join(', ')            
-        }))
-    displayPokemon(pokemon);
-    })
-    
+            type: data.types.map((type) => type.type.name),
+            stats: data.stats.map((stats) => stats.stat.name),
+            statsValue: data.stats.map((stats) => stats.base_stat)
+        };
+        actual = pokemon.id;
+        console.log(pokemon);
+        agregarPokemon(pokemon);
+
+      });
+
+};
+
+function crearNodosLista(value){
+    for(let i = 0; i < value; i++){
+        var node = document.createElement("li");
+        node.classList.add("nodoPokeLista");
+        idPokeLista.appendChild(node);
+    }
 }
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon.map(pokeman =>
-    `
-    <li class="card">
-        <img class="card-image" src="${pokeman.image}"/>
-        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-        <p class="card_subtitle">Type: ${pokeman.type}</p>
-    </li>
-    `
-    )
-    .join('');
-    pokedex.innerHTML = pokemonHTMLString;
+function elegirPoke (value){
+    var dato = capitalize(value);
+    fetchPokeData (apiRoot+dato);
 }
 
-fetchPokemon();
+function siguientePag (){
+    if (nextPage) {
+        fetchPokemonList(nextPage);
+    }
+}
+
+function anteriorPag (){
+    if (prevPage) {
+        fetchPokemonList(prevPage);
+    }
+}
+
+function anteriorPoke(){
+    if (actual > 1) {
+        actual--;
+        fetchPokeData(apiRoot+actual);
+    }
+}
+
+function siguientePoke(){
+    if (actual < 1118) {
+        actual++;
+        fetchPokeData(apiRoot+actual);
+    }
+}
+
+function buscarPoke(){
+    let dato = String(guessField.value);
+    fetchPokeData(apiRoot+dato);
+}
+
+crearNodosLista(limit);
+fetchPokeData(apiRoot+actual);
+fetchPokemonList(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`);
